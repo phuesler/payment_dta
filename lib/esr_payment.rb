0741fce4c0b1e57e8587ae42b6614d9958f9a532
Patrick Huesler <patrick.huesler@gmail.com>
2009-10-14 13:32:13 +0200

    finsished segment 1 for esr payment

--- lib/esr_payment.rb
+++ lib/esr_payment.rb
@@ -1,4 +1,40 @@
 require 'payment'
 class ESRPayment < Payment
 
+ protected
+ 
+ def build_segment1
+  super + reference_number + debit_account_number + debit_amount + reserve_field
+ end
+  
+ private
+ 
+ def reference_number
+  @data[:reference_number]
+ end
+ 
+ def debit_account_number
+  @data[:debit_account_number].ljust(24)
+ end
+ 
+ def debit_amount
+   debit_amount_valuta + debit_amount_currency + debit_amount_value
+ end
+ 
+ def debit_amount_valuta
+  " ".ljust(6)
+ end
+ 
+ def debit_amount_currency
+  @data[:debit_amount_currency]
+ end
+ 
+ def debit_amount_value
+  @data[:debit_amount].ljust(12)
+ end
+ 
+ def reserve_field
+  " ".ljust(14)
+ end
+
 end
--- spec/esr_payment_spec.rb
+++ spec/esr_payment_spec.rb
@@ -6,58 +6,98 @@ describe "ESR payment" do
     @default_attriburtes = {
       :file_identification => "ABC12",
       :sequence_number => 1,
-      :payers_clearing_number => "254"
+      :payers_clearing_number => "254",
+      :reference_number => 'ABC0100123478901',
+      :debit_account_number => '10235678',
+      :debit_amount_currency => 'CHF',
+      :debit_amount => '3949.75'
     }
   end
-  describe ESRPayment, "record" do
+  describe ESRPayment, "segment 1" do
     it "should set the segment field to 01" do
-      ESRPayment.new(@default_attriburtes).segment[0,2].should == "01"
+      ESRPayment.new(@default_attriburtes).segment1[0,2].should == "01"
     end
+    
+    it "should have a reference number" do
+      ESRPayment.new(@default_attriburtes.merge(:reference_number => 'ABC0100123478901')).segment1[53,16].should == "ABC0100123478901"
+    end
+    
+    it "should have a debit account without IBAN justified left filled with blanks" do
+      ESRPayment.new(@default_attriburtes.merge(:debit_account_number => '10235678')).segment1[69,24].should == '10235678                '
+    end
+    
+    it "should have a debit account with IBAN" do
+      ESRPayment.new(@default_attriburtes.merge(:debit_account_number => 'CH9300762011623852957')).segment1[69,24].should == 'CH9300762011623852957   '
+    end
+    
+    it "should have a blank debit amount valuta (6 blanks)" do
+      ESRPayment.new(@default_attriburtes).segment1[93,6].should == '      '
+    end
+    
+    it "should have a debit amount currency code" do
+     ESRPayment.new(@default_attriburtes.merge(:debit_amount_currency => 'CHF')).segment1[99,3].should == 'CHF'
+    end
+    
+    it "should have a debit amount justified left filled with blanks" do
+      ESRPayment.new(@default_attriburtes.merge(:debit_amount => '3949.75')).segment1[102,12].should == '3949.75     '
+    end
+    
+    it "should have a reserve field" do
+      ESRPayment.new(@default_attriburtes).segment1[114,14].should == " ".ljust(14) 
+    end
+    
+    it "should have a total length of 128 characters" do
+      ESRPayment.new(@default_attriburtes).segment1.size.should == 128
+    end
+    
+    describe "header" do
+      it "should have a total length of 51 characters" do
+        ESRPayment.new(@default_attriburtes).header.size.should == 51
+      end
+
+      it "should set a the correct execution date" do
+        ESRPayment.new(@default_attriburtes.merge(:execution_date => "051021")).header[0,6].should == "051021"
+      end
+
+      it "should fill the bc number with blanks" do
+        ESRPayment.new(@default_attriburtes).header[6,12].should == "".ljust(12," ")
+      end
+
+      it "should fill out a blank sequence number" do
+        ESRPayment.new(@default_attriburtes).header[18,5].should == "00000"
+      end
+
+      it "should set the creation date" do
+        ESRPayment.new(@default_attriburtes).header[23,6].should == Date.today.strftime("%y%m%d")
+      end
+
+      it "should set the payers clearing number and pad it" do
+        ESRPayment.new(@default_attriburtes.merge(:payers_clearing_number => "254")).header[29,7].should == "2540000"
+      end
+
+      it "should set the file identification" do
+        ESRPayment.new(@default_attriburtes.merge(:file_identification => "ABC12")).header[36,5].should == "ABC12"
+      end
+
+      it "should should set the record sequence number" do
+       ESRPayment.new(@default_attriburtes.merge(:record_sequence_number => 1)).header[41,5].should == "00001"
+      end
+
+      it "should should set the transaction type to 826" do
+       ESRPayment.new(@default_attriburtes).header[46,3].should == "826" 
+      end
+
+      it "should set the payment type to 1" do
+       ESRPayment.new(@default_attriburtes).header[49,1].should == "1" 
+      end
+
+      it "should set the transaction flag to 0" do
+       ESRPayment.new(@default_attriburtes).header[50,1].should == "0" 
+      end
+    end
+
   end
-  
-  describe ESRPayment, "header" do
-    it "should have a total length of 51 characters" do
-      ESRPayment.new(@default_attriburtes).header.size.should == 51
-    end
-
-    it "should set a the correct execution date" do
-      ESRPayment.new(@default_attriburtes.merge(:execution_date => "051021")).header[0,6].should == "051021"
-    end
-
-    it "should fill the bc number with blanks" do
-      ESRPayment.new(@default_attriburtes).header[6,12].should == "".ljust(12," ")
-    end
-
-    it "should fill out a blank sequence number" do
-      ESRPayment.new(@default_attriburtes).header[18,5].should == "00000"
-    end
-
-    it "should set the creation date" do
-      ESRPayment.new(@default_attriburtes).header[23,6].should == Date.today.strftime("%y%m%d")
-    end
-
-    it "should set the payers clearing number and pad it" do
-      ESRPayment.new(@default_attriburtes.merge(:payers_clearing_number => "254")).header[29,7].should == "2540000"
-    end
-
-    it "should set the file identification" do
-      ESRPayment.new(@default_attriburtes.merge(:file_identification => "ABC12")).header[36,5].should == "ABC12"
-    end
-
-    it "should should set the record sequence number" do
-     ESRPayment.new(@default_attriburtes.merge(:record_sequence_number => 1)).header[41,5].should == "00001"
-    end
-
-    it "should should set the transaction type to 826" do
-     ESRPayment.new(@default_attriburtes).header[46,3].should == "826" 
-    end
-
-    it "should set the payment type to 1" do
-     ESRPayment.new(@default_attriburtes).header[49,1].should == "1" 
-    end
-
-    it "should set the transaction flag to 0" do
-     ESRPayment.new(@default_attriburtes).header[50,1].should == "0" 
-    end
-  end  
+    
+  describe ESRPayment, "segment 2" do
+  end
 end
